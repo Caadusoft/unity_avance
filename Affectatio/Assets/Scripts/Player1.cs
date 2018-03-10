e46a0a0ac4b98f62d7d51b186876d3527275d8f3
Caadusoft <agence.cadusoft@gmail.com>
2018-03-10 16:58:58 +0100

    10/03/2018
    PV du personnage et mort si on sort de la caméra

---
+++ Affectatio/Assets/Scripts/Player1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public int Health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+    public int fall = -20;
+
+    void Update()
+    {
+            if (transform.position.y <= fall)
+            {
+            DamagePlayer(999999);
+            }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.Health -= damage;
+        if (playerStats.Health <= 0)
+        {
+            GameMaster.KillPlayer(this);
+        }
+    }
+}
