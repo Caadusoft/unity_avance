19f70c9f6c2efe3f2abc087411d85d9e85a9efb5
Caadusoft <agence.cadusoft@gmail.com>
2018-04-14 11:26:12 +0200

    14/04/2018
    Barre de santé

--- Affectatio/Assets/Scripts/Player1.cs
+++ Affectatio/Assets/Scripts/Player1.cs
@@ -7,11 +7,40 @@ public class Player1 : MonoBehaviour {
     [System.Serializable]
     public class PlayerStats
     {
-        public int Health = 100;
+        public int maxHealth = 100;
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+
+        }
+
+        public void Init()
+        {
+            curHealth = maxHealth;
+        }
     }
 
-    public PlayerStats playerStats = new PlayerStats();
+    public PlayerStats stats = new PlayerStats();
     public int fall = -20;
+
+    [SerializeField]
+    private StatusIndicator statusIndicator;
+
+     void Start()
+    {
+        stats.Init();
+        
+        if (statusIndicator == null)
+        {
+            Debug.Log("Pas d'indicateur de santé sur le héros");
+        }
+        else
+        {
+            statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
+        }
+    }
 
     void Update()
     {
@@ -20,7 +49,7 @@ public class Player1 : MonoBehaviour {
             DamagePlayer(999999);
             }
 
-        if (playerStats.Health <= 0)
+        if (stats.curHealth <= 0)
         {
             GameMaster.KillPlayer(this);
         }
@@ -29,16 +58,18 @@ public class Player1 : MonoBehaviour {
 
     public void takedmg(int degat)
     {
-        this.playerStats.Health = this.playerStats.Health - degat;
-        Debug.Log(this.playerStats.Health);
+        this.stats.curHealth = this.stats.curHealth - degat;
+        Debug.Log(this.stats.curHealth);
+        statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
     }
 
     public void DamagePlayer(int damage)
     {
-        playerStats.Health -= damage;
-        if (playerStats.Health <= 0)
+        stats.curHealth -= damage;
+        if (stats.curHealth <= 0)
         {
             GameMaster.KillPlayer(this);
         }
+        statusIndicator.SetHealth(stats.curHealth, stats.maxHealth);
     }
 }
